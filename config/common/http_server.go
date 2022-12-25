2de42309273457e12721ee58ca23501ca281cba0
Fedir Nepyivoda <fednep@gmail.com>
2022-12-25 20:36:13 +0200

    Adds common config for HTTP server

---
+++ config/common/http_server.go
@@ -0,0 +1,54 @@
+package config
+
+import (
+	"errors"
+
+	"github.com/fednep/goapilib/config"
+)
+
+// ServerConfig contains common config parameters
+// for configuring HTTP(S) server
+type ServerConfig struct {
+	Address  string
+	Port     int
+	UseTLS   bool
+	CertFile string
+	KeyFile  string
+
+	ReadTimeoutSec  int
+	WriteTimeoutSec int
+}
+
+func (cfg *ServerConfig) LoadEnvOverrides() {
+
+	// Server Configuration overrides
+	config.Env("HTTP_ADDRESS", &cfg.Address)
+
+	// If specified any of this, we'll use it (for backward compatibility)
+	config.EnvInt("HTTP_PORT", &cfg.Port)
+
+	config.EnvBool("HTTP_USE_TLS", &cfg.UseTLS)
+
+	config.EnvInt("HTTP_READ_TIMEOUT", &cfg.ReadTimeoutSec)
+	config.EnvInt("HTTP_WRITE_TIMEOUT", &cfg.WriteTimeoutSec)
+
+	if cfg.UseTLS {
+		config.Env("HTTP_CERT_FILE", &cfg.CertFile)
+		config.Env("HTTP_KEY_FILE", &cfg.KeyFile)
+	}
+}
+
+func (cfg *ServerConfig) IsValid() error {
+
+	if cfg.Port == 0 {
+		return errors.New("[server] need to have port configured")
+	}
+
+	if cfg.UseTLS {
+		if cfg.CertFile == "" || cfg.KeyFile == "" {
+			return errors.New("[server] set to use TLS but does not have 'certfile' or 'keyfile' configured")
+		}
+	}
+
+	return nil
+}
